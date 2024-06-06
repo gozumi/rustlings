ac9e36b8dbd0f5a42479b1991a9b9d9d751fdc07
Ian Anderson <ian.kirk.anderson@gmail.com>
2024-06-06 21:11:46 +0100

    Initial commit.

---
+++ exercises/primitive_types/primitive_types3.rs
@@ -0,0 +1,19 @@
+// primitive_types3.rs
+//
+// Create an array with at least 100 elements in it where the ??? is.
+//
+// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
+// for a hint.
+
+use std::i32;
+
+fn main() {
+    let a: [usize; 100] = core::array::from_fn(|i| i + 1);
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed")
+    }
+}
